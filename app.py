d5d033549298ad72c4e1aea713a4500a766678bc
Sohil Kaushal <sohilkaushal@gmail.com>
2018-06-12 23:47:17 +1000

    Implemented hashing function and started sigup

--- app.py
+++ app.py
@@ -1,7 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bcrypt import hashpw, gensalt
+from  databaseSetup import User, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def hashedPassword(password):
+    return hashpw(password, gensalt())
+
+
+def matchHash(hashed, password):
+    if hashpw(password, hashed) == hashed:
+        return True
+    else:
+        return False
+
 
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///blogDatabase.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/signup', method=['post'])
+def signup():
+    error = None
+    if request.method == "post":
+        passwd = request.form['passwd']
+        repasswd = request.form['repasswd']
+        email = request.form['email']
+        users = session.query(User).all()
+        for i in users:
+            if i.email == email:
+                error = "Pre registerd email"
+                return render_template('signup.html', error)
+        if passwd == repasswd:
+            hash = hashedPassword(passwd)
+            newUser = User(firstName=request.form['fname'], lastName=request.form['lname'],
+                           email=email, password=hash)
+            session.add(newUser)
+            session.commit()
+            
 
 if __name__ == '__main__':
     app.run()
--- databaseSetup.py
+++ databaseSetup.py
@@ -11,17 +11,18 @@ class User(Base):
     uID = Column(Integer, primary_key=True, autoincrement=True)
     firstName = Column(String(50), nullable=False)
     lastName = Column(String(50), nullable=False)
-    username = Column(String(50), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String(10000), nullable=False)
-    dateOfRegisteration = Column(DateTime, default=datetime.datetime.now)
+    dateOfRegistration = Column(DateTime, default=datetime.datetime.now)
 
 
 class Post(Base):
-    postID= Column(Integer, primary_key=True, autoincrement=True)
+    __tablename__ = 'post'
+    postID = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(1000))
     description = Column(String(10000))
     owner = Column(Integer, ForeignKey('user.uID'))
+
 
 engine = create_engine('sqlite:///blogDatabase.db')
 Base.metadata.create_all(engine)
